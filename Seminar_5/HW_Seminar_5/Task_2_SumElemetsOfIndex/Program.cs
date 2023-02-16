b4dd937a39eca9ea8ae596fef8d2a93c0120fe39
«Evgeniy <boka3006@gmail.com>
2023-02-16 20:00:39 +0300

    1502

---
+++ Seminar_5/HW_Seminar_5/Task_2_SumElemetsOfIndex/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+int SumElements(int[] newArray)
+{
+    int sum = 0;
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (i % 2 != 0) sum = sum + newArray[i];
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.WriteLine("Enter array size:");
+int size = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the minimum value:");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the maximum value:");
+int max = int.Parse(Console.ReadLine()!);
+int[] arrayTest = GetArray(size, min, max);
+
+
+Console.WriteLine($"{String.Join(", ", arrayTest)} -> {SumElements(arrayTest)}");
---
+++ Seminar_5/HW_Seminar_5/Task_3_massivVeshesNum/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+double[] array = new double[5];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = 5 - rand.Next(11) + rand.NextDouble();
+}
+
+
+double subtraction(double[] InArray)
+{
+    double min = InArray[0];
+    double max = InArray[0];
+    for (int i = 0; i < InArray.Length; i++)
+    {
+        if (InArray[i] < min) min = InArray[i];
+    }
+    for (int a = 0; a < InArray.Length; a++)
+    {
+        if (InArray[a] > max) max = InArray[a];
+    }
+    double sub = max - min;
+    return sub;
+}
+
+
+
+
+Console.WriteLine($"{String.Join("|", array)} -> {subtraction(array)}");
+
+

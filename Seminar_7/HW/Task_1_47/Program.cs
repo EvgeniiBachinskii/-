3ec6c958abd93c7d7869d63103139b143a56e83a
«Evgeniy <boka3006@gmail.com>
2023-02-28 09:26:54 +0300

    2802

---
+++ Seminar_7/HW/Task_1_47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+double[,] Get2DArray(int row, int col, double min, double max)
+{
+    double[,] array = new double[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = Math.Round(new Random().NextDouble() * (max - min) + min, 3);
+        }
+    }
+    return array;
+}
+
+void Print2dArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Print2dArray(Get2DArray(3,4,-5,5));
---
+++ Seminar_7/HW/Task_3_52/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+void CalculateArithmeticMeanInCol(int[,] array)
+{
+    int col = 0;
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    while (col < array.GetLength(1))
+    {
+        int row = 0;
+        double sum = 0;
+        while (row < array.GetLength(0))
+        {
+            sum += array[row, col];
+            row++;
+        }
+        Console.Write($" {Math.Round(sum / array.GetLength(0), 2)};");
+        col++;
+    }
+}
+
+int[,] newArray = new int[,]
+{
+{1, 4, 7, 2},
+{5, 9, 2, 3},
+{8, 4, 2, 4},
+};
+
+CalculateArithmeticMeanInCol(newArray);
